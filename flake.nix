036b1554435551bdebe0e5b1c994ac492896c453
b4er <basil.fuerer@protonmail.com>
2025-03-18 15:12:42 +0100

    fix haddock (?)

--- flake.nix
+++ flake.nix
@@ -76,7 +76,7 @@
           docs = {
             type = "app";
             program = "${pkgs.writeShellScript "publish" ''
-              nix develop -c sh -c 'cabal haddock-project --hackage --test --output=docs/haddock'
+              nix develop -c sh -c 'cabal update && cabal haddock-project --hackage --test --output=docs/haddock'
 
               # rewrite the links to hackage for recalc* Haddock
               for file in $(grep -rl 'https://hackage.haskell.org/package/recalc' docs/haddock); do
